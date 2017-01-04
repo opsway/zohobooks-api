60e168d5184e998019c5e8962f1b9238c31519de
Javier Spagnoletti <phansys@gmail.com>
2017-01-04 15:35:55 -0300

    Allow to use extra parameters

--- src/Api/Invoices.php
+++ src/Api/Invoices.php
@@ -9,21 +9,21 @@ class Invoices extends BaseApi
 
     public function applyCredits($invoiceId, $data)
     {
-        $response = $this->client->post(static::API_PATH.'/'.$invoiceId.'/credits', $this->organizationId, $data);
+        $this->client->post(static::API_PATH.'/'.$invoiceId.'/credits', $this->organizationId, $data);
 
         return true;
     }
 
     public function markAsSent($invoiceId)
     {
-        $response = $this->client->post(static::API_PATH.'/'.$invoiceId.'/status/sent', $this->organizationId);
+        $this->client->post(static::API_PATH.'/'.$invoiceId.'/status/sent', $this->organizationId);
 
         return true;
     }
 
     public function markAsVoid($invoiceId)
     {
-        $response = $this->client->post(static::API_PATH.'/'.$invoiceId.'/status/void', $this->organizationId);
+        $this->client->post(static::API_PATH.'/'.$invoiceId.'/status/void', $this->organizationId);
 
         return true;
     }
