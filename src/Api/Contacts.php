fa570e6a912e8687a9465fed5352e63888ca7c79
Pablo Gabriel Picatto <p.picatto@gmail.com>
2017-06-07 18:53:01 -0300

    Updated contact in order to use `BaseApi::getList()` method

--- src/Api/Contacts.php
+++ src/Api/Contacts.php
@@ -6,11 +6,4 @@ class Contacts extends BaseApi
 {
     const API_PATH = 'contacts';
     const API_KEY = 'contact';
-
-    public function getList(array $filters = [])
-    {
-        $response = $this->client->getList(static::API_PATH, $this->organizationId, $filters);
-
-        return $response['contacts'];
-    }
 }
