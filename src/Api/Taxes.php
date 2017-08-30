642d1c13822dcbf01fd68c4f49592ba449524670
Pablo Gabriel Picatto <p.picatto@gmail.com>
2017-08-30 12:00:29 -0300

    Fixed error on taxes that do not generate correctly routes and do not retrieve taxes list
    | Q             | A
    | ------------- | ---
    | Branch		| master
    | Bug fix?      | yes
    | New feature?  | no
    | BC breaks?    | no
    | Deprecations? | no
    | Tests pass?   | yes
    | Fixed tickets | n/a
    | License       | MIT
    | Doc PR        | n/a
    
    Fixed error on taxes that do not generate correctly routes and do not retrieve taxes list

--- src/Api/Taxes.php
+++ src/Api/Taxes.php
@@ -4,6 +4,18 @@ namespace OpsWay\ZohoBooks\Api;
 
 class Taxes extends BaseApi
 {
-    const API_PATH = 'taxes';
+    const API_PATH = 'settings/taxes';
     const API_KEY = 'tax';
+
+    /**
+     * @param array $filters
+     *
+     * @return ItemList
+     */
+    public function getList(array $filters = [])
+    {
+        $response = $this->client->getList(static::API_PATH, $this->organizationId, $filters);
+
+        return new ItemList($response[static::API_KEY.'es'], $response['page_context']);
+    }
 }
