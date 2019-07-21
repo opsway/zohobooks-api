e2a0b4ba241516434632c532b4463e08863d598d
johnyhunt <evnec@opsway.com>
2019-07-21 19:46:34 +0300

    Fix Inventory Adjustment api key

--- src/Api/ItemAdjustments.php
+++ src/Api/ItemAdjustments.php
@@ -5,5 +5,5 @@ namespace OpsWay\ZohoBooks\Api;
 class ItemAdjustments extends BaseApi
 {
     const API_PATH = 'inventoryadjustments';
-    const API_KEY = 'inventoryadjustment';
+    const API_KEY = 'inventory_adjustment';
 }
