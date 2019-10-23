6981db61ef32b3fdc5018eab5846610432fad2b7
mobixon <mobixon@gmail.com>
2019-10-23 09:21:50 +0300

    added refund for credit notes

--- src/Api/CreditNotes.php
+++ src/Api/CreditNotes.php
@@ -6,4 +6,11 @@ class CreditNotes extends BaseApi
 {
     const API_PATH = 'creditnotes';
     const API_KEY = 'creditnote';
+
+    public function refund($creditNoteId, $data)
+    {
+        $this->client->post(static::API_PATH.'/'.$creditNoteId.'/refunds', $this->organizationId, $data);
+
+        return true;
+    }
 }
