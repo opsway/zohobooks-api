7ba61cac84bbb49c7d7e5f752b4782dc6d2150f7
mobixon <mobixon@gmail.com>
2019-11-15 17:15:21 +0200

    fixes in refund endpoint for credit notes

--- src/Api/CreditNotes.php
+++ src/Api/CreditNotes.php
@@ -6,11 +6,12 @@ class CreditNotes extends BaseApi
 {
     const API_PATH = 'creditnotes';
     const API_KEY = 'creditnote';
+    const API_REFUND_KEY = 'creditnote_refund';
 
     public function refund($creditNoteId, $data)
     {
-        $this->client->post(static::API_PATH.'/'.$creditNoteId.'/refunds', $this->organizationId, $data);
+        $response = $this->client->post(static::API_PATH.'/'.$creditNoteId.'/refunds', $this->organizationId, $data);
 
-        return true;
+        return $response[static::API_REFUND_KEY];
     }
 }
